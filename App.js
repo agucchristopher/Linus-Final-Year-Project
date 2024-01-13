ace3f31b6fc1a6dddbff1d8ac9257e064c887812
Agu Christopher <aguchris740@gmail.com>
2024-01-13 23:30:01 +0100

    updates

--- App.js
+++ App.js
@@ -1,47 +1,8 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, Button, View } from "react-native";
+import { StyleSheet, Text, Button, View, TouchableOpacity } from "react-native";
 import { useEffect, useState } from "react";
 import Voice from "@react-native-voice/voice";
-
-/*
-expo init expo-speech-to-text
-cd expo-speech-to-text
-expo install @react-native-voice/voice expo-dev-client
-
-Add the following to your app.json, inside the expo section:
-"plugins": [
-  [
-    "@react-native-voice/voice",
-    {
-      "microphonePermission": "Allow Voice to Text Tutorial to access the microphone",
-      "speechRecognitionPermission": "Allow Voice to Text Tutorial to securely recognize user speech"
-    }
-  ]
-]
-
-If you don't have eas installed then install using the following command:
-npm install -g eas-cli
-
-eas login
-eas build:configure
-
-Build for local development on iOS or Android:
-eas build -p ios --profile development --local
-OR
-eas build -p android --profile development --local
-
-May need to install the following to build locally (which allows debugging)
-npm install -g yarn
-brew install fastlane
-
-After building install on your device:
-For iOS (simulator): https://docs.expo.dev/build-reference/simulators/
-For Android: https://docs.expo.dev/build-reference/apk/
-
-Run on installed app:
-expo start --dev-client
-
-*/
+// import Svg, { Path, Rect } from "react-native-svg";
 
 export default function App() {
   let [started, setStarted] = useState(false);
@@ -82,6 +43,7 @@ export default function App() {
       {started ? (
         <Button title="Stop Speech to Text" onPress={stopSpeechToText} />
       ) : undefined}
+      <TouchableOpacity></TouchableOpacity>
       {results.map((result, index) => (
         <Text key={index}>{result}</Text>
       ))}
